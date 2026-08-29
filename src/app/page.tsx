749067c547fef649bd2610034f4695ccb0253a90
sionchu <140263362+sionchu@users.noreply.github.com>
2026-08-30 05:21:02 +0900

    feat: add operating command center to main workspace

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { OwnerOpsApp } from "@/components/ownerops-app";
+import { OperatingCommandCenter } from "@/components/operating-command-center";
 import { AppStateProvider } from "@/state/app-state";
 
 export default function Home() {
   return (
     <AppStateProvider>
+      <OperatingCommandCenter />
       <OwnerOpsApp />
     </AppStateProvider>
   );
